f48a5de5e6e5d1b93c68e12b3ccd33f44876a24e
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 14:50:23 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0
-  
-  while array[count] do
-    if array[value_to_find]
+  while count < array.length do
+    if array[count] === value_to_find
       return count
     end
     count+=1
